f2ee1c78269ed654d025080eb92ff16fd0f6716a
Anjan Kasturi <anjan.kasturi@gmail.com>
2017-02-13 15:24:23 +0530

    checking rebase

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main (String args[])
 	{
 		System.out.println("This is the Hi Branch");
-		System.out.print("Conflict statement Github");
+		System.out.print("Conflict statement eclipse");
 	}
 
 }
